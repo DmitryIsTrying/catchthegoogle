f6d40fcd5de8aed4b023384dbc56e540efec3bfa
Dmitry <d2013.dima@mail.ru>
2025-01-24 01:59:14 +0500

    release

---
+++ ui/components/Start/Start.component.js
@@ -0,0 +1,15 @@
+import { start } from "../../../core/state-manager.js";
+
+export function StartComponent() {
+  const element = document.createElement("button");
+  element.classList.add("button");
+  element.classList.add("main-button");
+  element.append("START GAME");
+  render(element);
+
+  return { element };
+}
+
+async function render(element) {
+  element.addEventListener("click", start);
+}
---
+++ ui/index.js
@@ -0,0 +1,12 @@
+import { AppComponent } from "./components/App.component.js";
+
+const rootElement = document.getElementById("root");
+
+function renderApp() {
+  rootElement.innerHTML = "";
+
+  const appComponent = AppComponent();
+
+  rootElement.append(appComponent.element);
+}
+renderApp();
